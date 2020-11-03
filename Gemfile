10456634b02a8e6975f7a6448af7d204c76a79eb
stephen2790022 <toulouse.stephen1@gmail.com>
2020-11-03 15:43:30 +0100

    first commit

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.7.0'
 gem 'faker'
 gem 'table_print'
 gem 'dotenv-rails'
+gem 'simple_form'
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.4'
 # Use postgresql as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     after_create :welcome_send
     
     # admin_events () / foreign_key: 'admin_event_id' == la clef qui appartient au admins
